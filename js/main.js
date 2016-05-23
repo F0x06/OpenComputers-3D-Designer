26e73adb5ea2a8ba1e141899317065ca9e6453ce
F0x <mail@f0x.me>
2016-05-23 20:40:03 +0200

    Adde Nopey to contributors

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@
  *
  *      Kevin Velickovic <k.velickovic@gmail.com>
  *
+ * Contributor(s):
+ *
+ *      Nopey <golgothasTerror101@gmail.com>
+ *
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
